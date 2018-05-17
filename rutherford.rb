d0b257db857da21c34816378453af767c9745a9d
Katie <katieclong1@gmail.com>
2018-05-17 18:22:57 -0400

    Adds ability to have multiple water trackers
    This is done by passing the user token to water.rb which maintains a
    hash of the users with a tracker and their thread ID. The threads are
    killed upon the stop command.

--- rutherford.rb
+++ rutherford.rb
@@ -11,7 +11,7 @@ re = RutherError.new()
 print "Hello, I'm Rutherford. Currently running.\n"
 
 # New bot instance
-bot = Discordrb::Commands::CommandBot.new token: <TOKEN>
+bot = Discordrb::Commands::CommandBot.new token: 'TOKEN', prefix: '!'
 
 # Simple ping pong response. Good for testing functionality
 bot.command :ping do |event|
@@ -25,7 +25,7 @@ bot.command( :water, max_args: 1 ) do |_event, *args|
   # Turn the command into a string
   command = "#{ args.at(0).downcase }"
 
-  resp = wt.water(command)
+  resp = wt.water(command, _event.user)
  
   # Different response based on if you are stopping or starting
   # Also throws a general unknown_command() error if "start" or "stop"
@@ -33,7 +33,7 @@ bot.command( :water, max_args: 1 ) do |_event, *args|
   if resp == 0
     _event.respond 'Hi! I\'m going to start to reminding you to drink water. Hydration is healthy :grinning:'
   elsif resp == 1 
-    _event.respond 'Stopping water tracking'
+    _event.respond 'Stopping water tracking.'
   elsif resp == -1
     _event.respond re.unknown_command( command ) 
   end
